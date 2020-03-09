a155702940ced087fd6c6b5898398119d50f6815
Klaus Zerwes <rlsit@users.noreply.github.com>
2020-03-09 14:57:36 +0100

    FIXED 'Could not get zone : HTTP 404: Not found' error for new zones, seems the api changed w/ 4.2

--- powerdns_zone.py
+++ powerdns_zone.py
@@ -128,7 +128,7 @@ class PowerDNSClient:
 
     def get_zone(self, server, name):
         req = self.session.get(url=self._get_zone_url(server, name))
-        if req.status_code == 422:  # zone does not exist
+        if req.status_code in [404, 422]:  # zone does not exist
             return None
         return self._handle_request(req)
 
